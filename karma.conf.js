dc4ddbe7520bde8832be50a19f12ee5f0555f5ec
Patrick Chaves <patrick095@gmail.com>
2022-05-01 18:55:16 -0300

    added instanbul-badges-readme for show coverage in readme

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
         },
         coverageIstanbulReporter: {
             dir: require('path').join(__dirname, './coverage/time-ly-challenge-test'),
-            reports: ['html', 'lcovonly', 'text-summary'],
+            reports: ['html', 'lcovonly', 'json-summary'],
             fixWebpackSourcePaths: true,
         },
         coverageReporter: {
